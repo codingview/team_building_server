70f3bc2440a3b646b54755b2e87a665fbfff8b6f
ZhangRZ <occultskyrong@163.com>
2017-08-22 01:06:09 +0800

    更新 - 项目结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,5 @@ module.exports = {
         , es6: true
     }, globals: { // 全局变量
         GLO: true
-    }, rules: { // 语法规则,0>off,1>warning,2>error
-        eqeqeq: 1
-        , curly: 2
-        , strict: 2
     }, extends: "google" // 遵循google语法规范
 };
