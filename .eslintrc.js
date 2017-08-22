d4557801d55a395538610eae63cc30ed5e4539a1
zrz <occultskyrong@163.com>
2017-08-22 11:24:25 +0800

    更新 - eslint的配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,8 @@ module.exports = {
     }, globals: { // 全局变量
         GLO: true
     }, extends: "google" // 遵循google语法规范
+    , rules: { // 自定义规则
+        'comma-style': [1, 'first'] // 逗号放在下一行起始
+        , 'comma-dangle': [1, 'never'] // 禁止使用拖尾逗号
+    }
 };
--- README.md
+++ README.md
@@ -3,3 +3,14 @@ team building server
 
 ## 团队建设系统前端服务
 
+## 安装&启动项目
+
+- 安装`eslint`
+  ```
+  npm install -g eslint
+  ```
+- 安装`google-eslint`规则
+  ```
+  npm install -g eslint-config-google
+  ```
+- `WebStorm`配置`eslint` ......
