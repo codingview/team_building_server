0cf23681dbc204c52459c5d3be0c554b57f1ccf5
ZhangRZ <occultskyrong@163.com>
2017-09-05 00:27:40 +0800

    更新 - 登录页面

--- public/javascript/admin/login/index.js
+++ public/javascript/admin/login/index.js
@@ -6,4 +6,37 @@
 
 'use strict';
 
-const data=
+// 输出显示信息
+const printF = msg=>alert(msg)
+    , listener = {
+    // 表单提交
+    submit: ()=> {
+        $('#login_submit').one('click', ()=> {
+            const user_name = $('#user_name').val()
+                , pwd = $('#pwd').val();
+            if (user_name && pwd) {
+                $.ajax({
+                    type: 'post'
+                    , url: '/admin/login'
+                    , data: {user_name, pwd}
+                    , dataType: 'json'
+                    , success: data=> {
+
+                    }
+                });
+            } else {
+                printF('请输入账号和密码');
+                listener.submit(); // 重新绑定监听
+            }
+        });
+    }
+
+    // 初始化监听
+    , init: function () {
+        this.submit();
+    }
+};
+
+(function () {
+    listener.init();
+}());
