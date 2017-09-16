a3246667ed403beb6c9453b19f8e18239f23b65a
sky91.cn <occultskyrong@163.com>
2017-09-16 20:49:13 +0800

    更新 - 分类列表操作

--- public/javascript/admin/production/catalog.js
+++ public/javascript/admin/production/catalog.js
@@ -6,6 +6,13 @@
 
 'use strict';
 
+class Node {
+    constructor(node) {
+        this.id = node.id;
+        this.name = node.name;
+    }
+}
+
 /* 产品分类 */
 
 let zTree; // ztree的实例
@@ -13,22 +20,29 @@ let zTree; // ztree的实例
 const Data = {
     // 获取分类列表
     catalogs: ()=>new Promise((resolve, reject)=>
-        $.ajax({
-            url: '/admin/production/catalog'
-            , method: 'post'
-            , dataType: 'json'
-            , success: json=> {
-                if (json && 'status' in json && json.status > 0) {
-                    resolve(json.data);
-                } else {
-                    reject('message' in json ? json.message : '获取数据出错');
+            $.ajax({
+                url: '/admin/production/catalog'
+                , method: 'post'
+                , dataType: 'json'
+                , success: json=> {
+                    if (json && 'status' in json && json.status > 0) {
+                        resolve(json.data);
+                    } else {
+                        reject('message' in json ? json.message : '获取数据出错');
+                    }
+                }, error: e=> {
+                    console.error(e);
+                    reject('请求超时');
                 }
-            }, error: e=> {
-                console.error(e);
-                reject('请求超时');
-            }
-        })
+            })
     )
+
+    // zTree - 简化树结构
+
+    // zTree - 提取节点中数据
+    , getNode: node=> {
+
+    }
 };
 
 const Dom = {
@@ -49,7 +63,7 @@ const Dom = {
                     zTree.addNodes(treeNode, {
                         id: (100 + newCount)
                         , pId: treeNode.id
-                        , name: 'new node' + (newCount++)
+                        , name: '新的分类' + (newCount++)
                     });
                     return false;
                 });
@@ -59,6 +73,34 @@ const Dom = {
 
     // zTree - 隐藏增加按钮
     , removeHoverDom: (treeId, treeNode)=>$('#addBtn_' + treeNode.tId).unbind().remove()
+
+    // zTree - 遍历树结构
+    , getNode: node=> {
+        const _node = new Node(node);
+        if ('children' in node && node.children instanceof Array && node.children.length > 0) {
+            _node.children = [];
+            node.children.forEach(child=> {
+                _node.children.push(Dom.getNode(child));
+            });
+        } else {
+            return _node;
+        }
+    }
+
+    // zTree - 提取树结构
+    , getTree: tree=> {
+        let catalogs = [];
+        tree = tree[0].children; // 根节点无需使用，直接下一层
+        //console.info(tree);
+        tree.forEach(t=> {
+            console.info(1, t);
+            const node = Dom.getNode(t);
+            console.info(2, node);
+            //catalogs.push(node);
+        });
+        //console.info(catalogs);
+        return catalogs;
+    }
 
     // 生成分类树
     , catalogs: ()=> {
@@ -80,7 +122,6 @@ const Dom = {
                                 , showRenameBtn: (treeId, treeNode)=>!treeNode.noEditBtn
                                 , showRemoveBtn: (treeId, treeNode)=>!treeNode.noEditBtn
                                 // todo 拖拽
-                                
                             }
                         }, [{
                             name: '产品分类'
@@ -88,7 +129,7 @@ const Dom = {
                             , children: catalogs
                             , noEditBtn: true
                         }])
-            )
+        )
             .catch(e=>alert(e));
     }
 
@@ -98,8 +139,23 @@ const Dom = {
     }
 };
 
-const Listener = {};
+const Listener = {
+
+    // 监听保存按钮
+    setOneSubmit: ()=> {
+        $('#production_catalog_submit').one('click', ()=> {
+            Dom.getTree(zTree.getNodes());
+            Listener.setOneSubmit();
+        });
+    }
+
+    // 初始化监听
+    , init: function () {
+        this.setOneSubmit();
+    }
+};
 
 $(function () {
     Dom.init();
+    Listener.init();
 });
