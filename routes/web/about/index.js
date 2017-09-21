ade7b724509931a0790e978f6e4310b9e6cf1070
zrz <occultskyrong@163.com>
2017-09-21 15:10:01 +0800

    新增 - 关于我们页面

--- routes/web/about/index.js
+++ routes/web/about/index.js
@@ -7,19 +7,29 @@
 'use strict';
 
 const express = require('express')
-    , router = new express.Router();
+    , router = new express.Router()
+    , menuConfig = require('../../../config/about_menu')
+    , aboutService = require('../../../service/about');
 
 // 关于我们 - home页
 router.get('/', (req, res)=>res.redirect('/about/home'));
 
 // 关于我们 - 其他页面
-router.get('/:title', (req, res)=>
-    res.render('./web/about/view', {
-        active: 'about'
-        , title: '关于我们'
-        , menu: require('../../../config/about_menu')
-        , active_menu: req.params.title
-    })
-);
+router.get('/:title', (req, res)=> {
+    const title = req.params.title;
+    aboutService.read(title)
+        .then(rich_text=>res.render('./web/about/view', {
+            active: 'about'
+            , title: '关于我们'
+            , menu: menuConfig
+            , active_menu: req.params.title
+            , rich_text: rich_text
+        }))
+        // todo 读取错误页面
+        .catch(e=> {
+            GLO.error(e, -99, '读取关于我们发生错误');
+            res.redirect('/error?');
+        });
+});
 
 module.exports = router;
