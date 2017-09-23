84119dbe1efc429075cb4ef13ac2f8737fb76da6
junsion <junsionzhang@gmail.com>
2017-09-23 21:42:21 +0800

    修改注释

--- utils/global/index.js
+++ utils/global/index.js
@@ -8,30 +8,32 @@
 
 const packageConfig = require('../../package.json')
     , _ = {
-        ENV: process.env.NODE_ENV || 'development' // 环境变量
-        , LOGO_NAME: '新易途' // LOGO名称
-        , SYS_NAME: packageConfig.name // 项目名称
-        , SYS_VERSION: packageConfig.version // 项目版本
-        , general: {} // 全局内存存储变量，项目启动时将数据从表中读取到该变量内
-        , isDev: ()=>GLO.ENV === 'development' // 是否为开发环境
-        , isPro: ()=>GLO.ENV === 'production' // 是否为生产环境
-        , success: (data, count)=> { // 成功的返回
+        ENV: process.env.NODE_ENV || 'development' 	// 环境变量
+        , LOGO_NAME: '新易途' 				// LOGO名称
+        , SYS_NAME: packageConfig.name 			// 项目名称
+        , SYS_VERSION: packageConfig.version 		// 项目版本
+        , general: {} 					// 全局内存存储变量，项目启动时将数据从表中读取到该变量内
+        , isDev: ()=>GLO.ENV === 'development' 		// 是否为开发环境
+        , isPro: ()=>GLO.ENV === 'production' 		// 是否为生产环境
+        , success: (data, count)=> { 			// 成功的返回
             if (count) {
                 return {status: 1, message: '', data: data, count: count};
             } else {
                 return {status: 1, message: '', data: data};
             }
         }
-        // 错误返回并记录相关信息
-        , error: (error, status = -1, msg = '')=> {
-            if (typeof error === 'string') { // error为错误信息时直接输出
+        					
+        , error: (error, status = -1, msg = '')=> {	// 错误返回并记录相关信息
+	    // error为错误信息时直接输出
+            if (typeof error === 'string') { 
                 return {
                     status: status
                     , message: error
                     , data: null
                 };
+	    // 记录错误信息
             } else {
-                GLO.logger('error').error(error);// 记录错误信息
+                GLO.logger('error').error(error);
                 return {
                     status: status
                     , message: msg
