3012f0d023290535c358100eca23d35d750a0370
ZhangRZ <occultskyrong@163.com>
2017-12-19 21:12:21 +0800

    更新 - 服务案例展示效果

--- routes/api/example/index.js
+++ routes/api/example/index.js
@@ -1,32 +1,30 @@
 /**
- * Created by zhangrz on 9月17.
+ * Created by zhangrz on 2017/12/13.
  * Copyright© 2015-2020 occultskyrong (https://github.com/occultskyrong)
  * @version 0.0.1 created
  */
 
-'use strict';
-
 const express = require('express')
     , router = new express.Router()
-    , productionService = require('../../../service/production');
+    , exampleService = require('../../../service/example');
 
-// 根据分类获取商品
+// 根据分类获取服务案例
 router.post('/list', (req, res)=> {
     const body = req.body;
     let _ = {
         offset: 'offset' in body ? parseInt(body.offset) : 0
         , limit: 'limit' in body ? parseInt(body.limit) : 20
     };
-    if ('sci' in body) {
-        const sci = parseInt(body.sci);
-        if (sci !== -1) {
-            _.second_catalog_id = sci;
+    if ('cid' in body) {
+        const cid = parseInt(body.cid);
+        if (cid !== -1) {
+            _.catalog_id = cid;
         }
-        productionService.production.listBySci(_)
+        exampleService.listByCid(_)
             .then(data=>res.json(GLO.success(data.data, data.count)))
-            .catch(e=>res.json(GLO.error(e, -99, '根据分类获取商品出错')));
+            .catch(e=>res.json(GLO.error(e, -99, '根据分类获取案例出错')));
     } else {
-        return res.json(GLO.error('未获取到分类编号:sci', -11));
+        return res.json(GLO.error('未获取到案例编号:cid', -11));
     }
 });
 
